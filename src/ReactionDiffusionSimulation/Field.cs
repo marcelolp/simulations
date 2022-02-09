4e988e5dcb537950a16e2f3bf0a7513fb2bf3438
Marcel Olp <marcel.olp.99@gmail.com>
2022-02-09 10:35:36 +0100

    added basis for rd-simulation

---
+++ src/ReactionDiffusionSimulation/Field.cs
@@ -0,0 +1,125 @@
+﻿using SimMath;
+using System;
+using System.Threading.Tasks;
+
+namespace ReactionDiffusionSimulation
+{
+    /// <summary>
+    /// Solves a simple wave equation numerically
+    /// A couple of examples for interesting source functions and initial conditions can be found in the corresponding sections of the code
+    /// </summary>
+    internal class Field
+    {
+        internal float[] Color
+        {
+            get { return _color; }
+            private set { _color = value; }
+        }
+        internal int NX { get; private set; }       // #gridpoints in x-direction
+        internal int NY { get; private set; }       // #gridpoints in y-direction
+
+        private bool _first = true;                 // first time step has to be treated differently as _u_last = undefined
+
+        private float _dx = 0.01f;                  // resolution of 1 step in the x-direction
+        private float _dy = 0.01f;                  // resolution of 1 step in the y-direction
+        private float _dt;                          // time step size
+        private float _t = 0.0f;                    // total time passed
+
+        private float[] _color;                     // contains (r,g,b)-color values for all gridpoints
+        private float[] _u;                         // contains the simulation grid at the current timestep
+        private float[] _u_last;                    // contains the simulation grid at the last timestep
+        private float[] _u_temp;                    // contains the simulation grid for the next timestep
+
+        private readonly Random _rng = new();
+
+        /// ==================================== Initial condition function ======================================= ///
+        (float, float) I(int x, int y) => (x, y);
+        /// ======================================== Feed function for u ========================================== ///
+        float F(int x, int y, float u, float v, float t) => 0.1f;
+        /// ======================================== Kill function for v ========================================== ///
+        float K(int x, int y, float u, float v, float t) => 0.1f;
+        /// ================================= Reaction ratio between u and v ====================================== ///
+        float R = 2.0f; // #u + R#v -> (1+R)#v
+        /// =================================== Diffusion rate for u and v ======================================== ///
+        float Du = 1.0f;
+        float Dv = 0.5f;
+
+        /// <summary>
+        /// Creates a simulation grid of the given size and timestep (default dt = 0.1)
+        /// </summary>
+        internal Field(int nX, int nY, float dt = 0.1f)
+        {
+            NX = nX;
+            NY = nY;
+            _dt = dt;
+            _u = new float[NX * NY];
+            _u_last = new float[NX * NY];
+            _u_temp = new float[NX * NY];
+            _color = new float[NX * NY * 3];
+
+            Init();
+        }
+
+        /// <summary>
+        /// Advances the simulation by a predefined timestep
+        /// Variable adt contains the actually used timestep
+        /// </summary>
+        internal void Iterate(out float adt)
+        {
+            Iterate(_dt, out adt);
+        }
+
+        /// <summary>
+        /// Advances the simulation by the given timestep
+        /// Timestep may be overwritten if it is to large for a stable simulation
+        /// Variable adt contains the actually used timestep
+        /// </summary>
+        internal void Iterate(float dt, out float adt)
+        {
+
+            // avoid repeated calls to Properties NX, NY
+            _t += dt;
+            int NX = this.NX;
+            int NY = this.NY;
+
+            // incase dt has changed, return the simulated timestep
+            adt = dt;
+
+            Parallel.For(0, NY, (y) =>
+            {
+
+                for (int x = 0; x < NX; x++)
+                {
+                    
+
+                }
+            });
+
+            // cycle through the grids to avoid creating a new one every iteration
+            float[] _tmp;
+            _tmp = _u_last;
+            _u_last = _u;
+            _u = _u_temp;
+            _u_temp = _tmp;
+            _first = false;
+        }
+
+
+        /// <summary>
+        /// Function to initialize the field
+        /// </summary>
+        private void Init()
+        {
+            for (int y = 0; y < NY; y++)
+            {
+                for (int x = 0; x < NX; x++)
+                {
+                    int idx = (3 * x) + (3 * y) * NX;
+                    (_color[0 + idx], _color[1 + idx], _color[2 + idx]) = (0.0f, 0.0f, 0.0f);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ src/ReactionDiffusionSimulation/Window.cs
@@ -0,0 +1,186 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactionDiffusionSimulation
+{
+    internal class Window : GameWindow
+    {
+        private const bool SAVE_TO_FILE = false;
+        private const bool USE_REAL_TIME = false;           // simulation runs as fast as it can while stying stable, realtime is not really possible
+        private const int SIM_WIDTH = 1600 / 2;
+        private const int SIM_HEIGHT = 900 / 2;
+        private const float FIXED_DELTA = 0.001f;
+        private const float MAX_FRAMERATE = 30.0f;
+        private const float MIN_DELTA = 1000.0f / MAX_FRAMERATE;
+
+        private int _vertexBufferObject;
+        private int _vertexColorBufferObject;
+        private int _vertexArrayObject;
+        private int _elementBufferObject;
+
+        private float _sim_time;
+
+        private Shader _shader;
+        private Field _field;
+
+        private readonly Stopwatch sim_delta;
+        private readonly float[] _vertices;
+        private readonly uint[] _indices;
+        private readonly float[] _colors;
+
+
+        internal Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+            VSync = VSyncMode.Adaptive;
+            RenderFrequency = MAX_FRAMERATE;
+            UpdateFrequency = USE_REAL_TIME ? 0.0 : MAX_FRAMERATE;
+
+            _field = USE_REAL_TIME ? new(nX: SIM_WIDTH, nY: SIM_HEIGHT) : new(nX: SIM_WIDTH, nY: SIM_HEIGHT, dt: FIXED_DELTA);
+            _colors = _field.Color;
+            _vertices = new float[_field.NX * _field.NY * 3];
+            _indices = new uint[(_field.NX - 1) * (_field.NY - 1) * 6];
+
+
+            // screen goes from (-1, -1) to (1, 1)
+            float deltaX = 2.0f / (_field.NX - 1.0f);
+            float deltaY = 2.0f / (_field.NY - 1.0f);
+
+            // vertex coordinates on screen
+            for (int y = 0; y < _field.NY; y++)
+            {
+                for (int x = 0; x < _field.NX; x++)
+                {
+                    _vertices[0 + (3 * x) + _field.NX * (3 * y)] = x * deltaX - 1.0f;   // x-coord
+                    _vertices[1 + (3 * x) + _field.NX * (3 * y)] = y * deltaY - 1.0f;   // y-coord
+                    _vertices[2 + (3 * x) + _field.NX * (3 * y)] = 0.0f;                // z-coord
+                }
+            }
+
+            // indices for connecting vertices to triangles to create a screen-size grid
+            for (int y = 0; y < _field.NY - 1; y++)
+            {
+                for (int x = 0; x < _field.NX - 1; x++)
+                {
+                    int index = (6 * x) + (_field.NX - 1) * (6 * y);
+
+                    _indices[0 + index] = (uint)(x + _field.NX * y); //ul-vertex, 1st tri
+                    _indices[1 + index] = (uint)((x + 1) + _field.NX * y); //ur-vertex, 1st tri
+                    _indices[2 + index] = (uint)(x + _field.NX * (y + 1)); //ll-vertex, 1st tri
+                    _indices[3 + index] = (uint)((x + 1) + _field.NX * (y + 1)); //lr-vertex, 2nd tri
+                    _indices[4 + index] = (uint)((x + 1) + _field.NX * y); //ur-vertex, 2nd tri
+                    _indices[5 + index] = (uint)(x + _field.NX * (y + 1)); //ll-vertex, 2nd tri
+                }
+            }
+
+            sim_delta = Stopwatch.StartNew();
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            // =============================================== VBOs ===================================================
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _vertexColorBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexColorBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _colors.Length * sizeof(float), _colors, BufferUsageHint.StreamDraw);
+
+            // ============================================ Attributes ================================================
+
+            GL.EnableVertexAttribArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);  // vertices, three elements X, Y, Z
+
+            GL.EnableVertexAttribArray(1);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexColorBufferObject);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);  // colors, three elements R, G, B
+
+            // =============================================== EBO ====================================================
+
+            _elementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
+            _shader = new Shader("../../../Shaders/shader.vert", "../../../Shaders/default.frag");
+            _shader.Use();
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            _shader.Use();
+
+            GL.Uniform1(GL.GetUniformLocation(_shader.Handle, "time"), _sim_time);
+
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            SwapBuffers();
+
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+            else if (KeyboardState.IsKeyDown(Keys.Space))     // pause simulation while pressing spacebar 
+            {
+                return;
+            }
+
+            long delta = sim_delta.ElapsedMilliseconds;
+            sim_delta.Restart();
+
+
+            float adt;
+            if (USE_REAL_TIME)
+                _field.Iterate(delta / 1000.0f, out adt);
+            else
+                _field.Iterate(out adt);
+            _sim_time += adt;
+            Title = $"{1.0f / (delta / 1000.0f):0.00} fps - {_sim_time:0.00} seconds";
+
+            // Colors have to be updated every simulation step
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexColorBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _colors.Length * sizeof(float), _colors, BufferUsageHint.StreamDraw);
+
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+
+    }
+}
